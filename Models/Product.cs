c84163b2d9e761c7bfc0cee93a8ee63ca0ec7dc6
edipojuan <edipojs@gmail.com>
2019-08-11 23:56:54 -0400

    Updated Models/Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Store.Models
+{
+  public class Product
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreateDate { get; set; }
+  }
+}

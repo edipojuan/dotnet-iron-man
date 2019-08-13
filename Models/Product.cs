111835dad2a93ab45c0308f9d7ba9f1e7577549e
edipojuan <edipojs@gmail.com>
2019-08-13 01:03:53 -0400

    Models Atualizadas

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Store.Models
+namespace ProductCatalog.Models
 {
   public class Product
   {
@@ -9,6 +9,10 @@ namespace Store.Models
     public string Description { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
+    public string Image { get; set; }
     public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
   }
 }

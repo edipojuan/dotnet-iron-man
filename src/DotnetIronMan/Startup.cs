c0b9f812837a7ebdd35d3bbb73871b49d373bc4c
edipojuan <edipojs@gmail.com>
2019-08-26 23:26:20 -0400

    Renamed dotnet to DotnetIronMan

--- src/DotnetIronMan/Startup.cs
+++ src/DotnetIronMan/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Store.Repositories;
 
-namespace dotnet
+namespace DotnetIronMan
 {
   public class Startup
   {
